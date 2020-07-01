6f34fad8cc72aea16863b36665f1bad19289b0dd
Novecento <ivan.monfrini@gmail.com>
2020-07-01 16:27:46 +0200

    csaicjaisj

--- js/script.js
+++ js/script.js
@@ -1,45 +1,52 @@
 $(document).ready(function () {
 
     $(document).on('click', '#btn_ricerca', function() {
-
+        $('#stampa_risultato').text('');
         var valoreInput = $('#input_ricerca').val();
-        console.log(valoreInput);
         stampaFilm(valoreInput);
+        $('#input_ricerca').val('');
     });
+    $('#input_ricerca').keypress(
+            function(event) {
+            if(event.which === 13 || event.keycode === 13) {
+                $('#stampa_risultato').text('');
+                var valoreInput = $('#input_ricerca').val();
+                stampaFilm(valoreInput);
+                $('#input_ricerca').val('');
+            }
+        });
 
-    function stampaFilm(arrayRisultato) {
+    function stampaFilm(queryRisultato) {
         $.ajax({
             url:"https://api.themoviedb.org/3/search/movie",
             method:"GET",
             data:{
                 api_key:"1b18b71d8924533ba2d9001b208ddde7",
-                query:arrayRisultato,
+                query:queryRisultato,
                 language:"it-IT"
             },
             success:function(data) {
+                var source = $("#film-template").html();
+                var template = Handlebars.compile(source);
 
-                    var source = $("#film-template").html();
-                    var template = Handlebars.compile(source);
+                var arrayRisultato = data.results;
+                for (var i = 0; i < arrayRisultato.length; i++) {
+                    var singoloRisultato = arrayRisultato[i];
+                    var titoloFilm = singoloRisultato.title;
+                    var votoFilm = singoloRisultato.vote_average;
+                    var titoloOriginaleFilm = singoloRisultato.original_title;
+                    var linguaOriginaleFilm = singoloRisultato.original_language;
 
-                    var arrayRisultato = data.results;
-                    for (var i = 0; i < arrayRisultato.length; i++) {
-                        var singoloRisultato = arrayRisultato[i];
-                        var titoloFilm = singoloRisultato.title;
-                        var votoFilm = singoloRisultato.vote_average;
-                        var titoloOriginaleFilm = singoloRisultato.original_title;
-                        var linguaOriginaleFilm = singoloRisultato.original_language;
+                    var context = {
+                        title:titoloFilm,
+                        original_title:titoloOriginaleFilm,
+                        original_language:linguaOriginaleFilm,
+                        vote_average:votoFilm
+                    };
+                    var html = template(context);
+                    $('#stampa_risultato').append(html);
 
-                        var context = {
-                            title:titoloFilm,
-                            original_title:titoloOriginaleFilm,
-                            original_language:linguaOriginaleFilm,
-                            vote_average:votoFilm
-                        };
-                        var html = template(context);
-                        $('#stampa_risultato').append(html);
-
-                    }
-
+                }
             },
             error:function() {
                 alert('Errore');
@@ -48,11 +55,5 @@ $(document).ready(function () {
         });
 
     };
-
-
-
-
-
-
 
 });
